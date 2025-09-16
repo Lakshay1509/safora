534321daa4d1d7e884452c029c41d3a8af019656
Lakshay <gupta15.lakshay@gmail.com>
2025-09-16 11:36:52 +0530

    fix: enhance query enabling conditions in useGetFollowingPost and useGetUpVotesByUser hooks

--- features/community/use-get-post-folllowing.ts
+++ features/community/use-get-post-folllowing.ts
@@ -1,12 +1,16 @@
 import {useQuery} from "@tanstack/react-query";
 import {client} from "@/lib/hono"
+import { useAuth } from "@/contexts/AuthContext";
 
 type Props = {
     limit?: string;
 }
 
+
 export const useGetFollowingPost = ({ limit }: Props)=>{
+    const {user, loading} = useAuth();
     const query = useQuery({
+        enabled :!!user && !loading,
         queryKey: ["following-post", { limit }],
         queryFn: async ()=>{
             const response = await client.api.community.following.$get({
--- features/votes/use-get-upvotes-byUser.ts
+++ features/votes/use-get-upvotes-byUser.ts
@@ -1,10 +1,12 @@
 import {useQuery} from "@tanstack/react-query";
 import {client} from "@/lib/hono"
+import { useAuth } from "@/contexts/AuthContext";
 
 
 export const useGetUpVotesByUser = (id:string)=>{
+    const {user, loading} = useAuth();
     const query = useQuery({
-        enabled :!!id,
+        enabled :!!id && !!user && !loading,
         queryKey: ["upvotes-by-user",id],
         queryFn: async ()=>{
             const response = await client.api.upvotes[":id"].$get({
